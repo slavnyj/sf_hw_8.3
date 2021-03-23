f245eccb6a5feda3a26c47f55d77b3301bdd7903
Alexandr Ivanov <slavnyj.s@gmail.com>
2021-03-23 20:39:40 +0600

    Added Swiftmailer via Composer, and added code for sent email message.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>This is PHP Sample Project</h1>
+
+<?php
+echo "ЗАДАНИЕ 8.3.4. Александр Иванов, DEVOPS-7";
+
+
+require_once '/path/to/vendor/autoload.php';
+
+// Create the Transport
+$transport = (new Swift_SmtpTransport('smtp.example.org', 25))
+  ->setUsername('your username')
+  ->setPassword('your password')
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message('Wonderful Subject'))
+  ->setFrom(['john@doe.com' => 'John Doe'])
+  ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
+  ->setBody('Here is the message itself')
+  ;
+
+// Send the message
+// $result = $mailer->send($message);
+?> 
+
+</body>
+</html>
